d116166c1841625cd0cc000eee22715dee4b5acf
maayachi <maayachi@sqli.com>
2019-04-05 23:21:20 +0100

    cleaning pom

--- rating/src/main/java/com/microservice/rating/controllers/RatingsController.java
+++ rating/src/main/java/com/microservice/rating/controllers/RatingsController.java
@@ -1,16 +1,11 @@
 package com.microservice.rating.controllers;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.apache.http.util.Asserts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;

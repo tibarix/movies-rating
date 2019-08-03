fd3a10e774b4cd382720c5a3feffe2f1b439789c
maayachi <maayachi@sqli.com>
2019-08-03 17:03:38 +0100

    docker-ready eureka-server and config-server

--- eureka-server/src/main/java/com/microservice/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/microservice/eurekaserver/EurekaServerApplication.java
@@ -1,13 +1,17 @@
 package com.microservice.eurekaserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
